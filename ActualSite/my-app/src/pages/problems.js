be1759e6174cba89f138af57ab165101c7ef2422
detonator707 <149191513+FinKrug@users.noreply.github.com>
2026-07-07 12:19:59 -0600

    added css

--- ActualSite/my-app/src/pages/problems.js
+++ ActualSite/my-app/src/pages/problems.js
@@ -1,18 +1,39 @@
 import { Link } from "react-router-dom";
 import problems from "../data/problemData";
 
+const weeks = [
+  {
+    week: "Week 1",
+    challenges: [
+      {
+        id: 1,
+        title: "Reverse String",
+        difficulty: "Easy"
+      },
+      {
+        id: 2,
+        title: "Two Sum",
+        difficulty: "Medium"
+      }
+    ]
+  }
+];
+
 function Problems() {
   return (
     <div className="container">
+  <h1>Weekly Challenges</h1>
 
-      <h1>Challenges</h1>
+  {weeks.map((week, index) => (
+    <div key={index} className="week-card">
 
-      {problems.map(problem => (
+      <h2>{week.week}</h2>
+
+      {week.challenges.map((problem) => (
         <div
           key={problem.id}
           className="problem-card"
         >
-
           <h3>{problem.title}</h3>
 
           <p>{problem.difficulty}</p>
@@ -20,11 +41,12 @@ function Problems() {
           <Link to={`/problems/${problem.id}`}>
             Open Problem
           </Link>
-
         </div>
       ))}
 
     </div>
+  ))}
+</div>
   );
 }
 
